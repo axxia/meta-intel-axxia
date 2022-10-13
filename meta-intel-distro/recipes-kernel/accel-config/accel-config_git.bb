axxia/meta-intel-axxia

commit 26665e3b7ca304189b37fdfc6cee24e9a9a06cb6

accel-config: Temporary disable documentation build
Documentation fails to install without a clear error message:
Makefile:574: recipe for target 'accel-config.1' failed
| make[2]: *** [accel-config.1] Error 1
| make[2]: *** Waiting for unfinished jobs....
| Makefile:574: recipe for target 'accel-config-enable-device.1' failed
| Makefile:574: recipe for target 'accel-config-load-config.1' failed

This seems to be caused by a change in libtool package but can't figure
out without a clear error message.

Signed-off-by: Daniel Dragomir <daniel.dragomir@windriver.com>

meta-intel-distro/recipes-kernel/accel-config/accel-config_git.bb
@@ -24,7 +24,7 @@ S = "${WORKDIR}/git"
 
 inherit autotools-brokensep gettext
 
-EXTRA_OECONF_append = " --enable-test=yes"
+EXTRA_OECONF_append = " --enable-test=yes --disable-docs"
 
 do_configure_prepend() {
 	(cd ${S}; ./autogen.sh; cd -)

commit f3fa5d6137219d5b2f48e294badd4ea5ae2470bf

accel-config: Upgrade to 3.4.8 and fix configure error
Inheriting pkgconfig class to ensure pkgconfig macros are
present for autoreconf in order to fix the following error:
| configure.ac:40: error: possibly undefined macro: AC_DEFINE

This also fix the documentation instalation which was failing
and was skiped in commit 26665e3b7c (" accel-config: Temporary
disable documentation build").

This commit also rename the license name to SPDX style licensing
format to avoid the following warning:
WARNING: accel-config-3.4.1-r0 do_package_qa: QA Issue: Recipe
LICENSE includes obsolete licenses GPL-2.0 LGPL-2.1 [obsolete-license]

Signed-off-by: Daniel Dragomir <daniel.dragomir@windriver.com>

meta-intel-distro/recipes-kernel/accel-config/accel-config_git.bb
@@ -8,23 +8,23 @@ to configure resources."
 HOMEPAGE = "https://github.com/intel/idxd-config"
 SECTION = "libs"
 
-LICENSE = "GPL-2.0 & LGPL-2.1"
+LICENSE = "GPL-2.0-only"
 LIC_FILES_CHKSUM = "file://LICENSE_GPL_2_0;md5=fa69eb765efdca4a83cd19e915db9ab0"
 
 DEPENDS = "asciidoc-native xmlto-native util-linux json-c kmod udev"
 
 RDEPENDS:${PN} = "bash"
 
-SRCREV = "ec787d78dfbc65e501cc53a4151402f7ad7ec138"
-PV = "3.4.1"
+SRCREV = "7f76d467cace664df928f3d5ea4598b8d0cbc32b"
+PV = "3.4.8"
 
 SRC_URI = "git://github.com/intel/idxd-config.git;protocol=https;branch=stable"
 
 S = "${WORKDIR}/git"
 
-inherit autotools-brokensep gettext
+inherit autotools-brokensep pkgconfig gettext 
 
-EXTRA_OECONF:append = " --enable-test=yes --disable-docs"
+EXTRA_OECONF:append = " --enable-test=yes"
 
 do_configure:prepend() {
 	(cd ${S}; ./autogen.sh; cd -)